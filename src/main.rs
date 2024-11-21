4cc10c6ae338afc188711b5dd097a95a0d665565
zimrustpro <zimrustpro@gmail.com>
2024-11-21 14:21:24 +0200

    Wrap type in a tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+struct File(String);
+
 fn main() {
-    println!("Hello, world!");
+    let my_file = File(String::from("I am file contents"));
+    let my_string = String::from("I am file contents");
+    println!("{}", my_file.0 == my_string);
 }
