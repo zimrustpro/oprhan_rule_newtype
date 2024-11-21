e6ff621a04052a41f8f772a6bfd245b25c1f9638
zimrustpro <zimrustpro@gmail.com>
2024-11-21 14:35:06 +0200

    impl Display for custom File type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+#[derive(Clone, Debug)]
 struct File(String);
 
+impl std::fmt::Display for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let as_bytes = format!("{:?}", &self.0.as_bytes());
+        write!(f, "{as_bytes}")
+    }
+}
+
 fn main() {
-    let my_file = File(String::from("I am file contents"));
-    let my_string = String::from("I am file contents");
-    println!("{}", my_file.0 == my_string);
+    let file = File(String::from("I am file contents"));
+    println!("{file:?}");
+    println!("{file}");
 }
